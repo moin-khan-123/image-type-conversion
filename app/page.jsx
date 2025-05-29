d2271536f2f858016d0087ad7755a6b1d7b514cd
Moin Khan <moin7657467@gmail.com>
2025-05-30 00:11:00 +0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { ImageConfigContext } from "next/dist/shared/lib/image-config-context.shared-runtime";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-6xl">Hello rasel bhalo aso</h1>
+    </div>
+  );
+}
